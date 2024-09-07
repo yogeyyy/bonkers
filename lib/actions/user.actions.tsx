93b5d9140221da789f5fcecd262e19d4b74cc635
Yogit Nainani <nainaniyogit@gmail.com>
2024-09-07 21:51:29 +0530

    Add: SignUp functionality using appwrite

--- lib/actions/user.actions.tsx
+++ lib/actions/user.actions.tsx
@@ -1,17 +1,55 @@
-'use server';
+"use server";
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+import { parse } from "path";
 
 export const signIn = async () => {
-    try {
-        //Mutation / Database / Make Fetch
-    } catch (error) {
-        console.error('Error', error);
-    }
-}
+  try {
+    //Mutation / Database / Make Fetch
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
 
 export const signUp = async (userData: SignUpParams) => {
-    try {
-        // Create a user account
-    } catch (error) {
-        console.error('Error', error);
-    }
+  const { email, password, firstName, lastName } = userData;
+  try {
+    // Create a user account
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
+
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+
+    return parseStringify(user);
+  } catch (error) {
+    return null;
+  }
 }
